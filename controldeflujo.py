f80fc0b84a3c5838855f91f989cd5f3a0b2e0634
DayronYaret <dayronyaret@gmail.com>
2020-02-17 10:00:31 +0000

    Primer comit desde el portatil, primeros for

--- controldeflujo.py
+++ controldeflujo.py
@@ -20,13 +20,26 @@ else:
 edad = 0
 while edad <18:
     edad = edad + 1
+    ##if edad % 2 == 0: con esto solo imprime el mensaje en numeros impares
+      ##  continue
     print("Ahora mismo tienes " + str(edad))
 
 print("habla contigo mismo, para salir escriba venga puto")
 while True:
     entrada = input()
     if entrada == "venga puto":
+        print("se terminó la conversación")
         break
     else:
         print(entrada)
-#TODO pagina 33 de 160
+
+secuencia = ["uno", "dos", "tres"]
+for elemento in secuencia:
+    print(elemento)
+
+"""range crea una lista con numeros desde el inicio que es 30 hasta 50 que es el final, el bucle consiste en hacer print al valor que toma i en cada iteracion del
+for, pero acaba en 49, por lo que para llegar a 50 lo hago de forma manual"""
+for i in range(30, 50):
+    print(i)
+    if i == 49:
+        print("50")
